167bf02ed1121877a547663a5e7a1cb04cb70335
John DiPiazza <johndapizza@gmail.com>
2026-02-26 11:27:41 -0500

    install firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import {app as firebaseApp} from './assets/firebase';
+
+declare global {
+  interface Window {
+    firebaseApp: typeof firebaseApp
+  }
+}
+
+window.firebaseApp = firebaseApp  // Expose the Firebase app instance for debugging purposes
 
 const rootElement = document.getElementById('root')
 
